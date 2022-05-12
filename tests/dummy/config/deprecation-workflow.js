29f1d06f96ad452ddfcdae2542792303fae1623b
Kevin Pfefferle <kevin@pfefferle.me>
2022-05-12 13:57:30 -0400

    Cleanup

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -1,10 +1,5 @@
 /* eslint-disable no-undef */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    {
-      handler: 'silence',
-      matchId: 'ember-views.curly-components.jquery-element',
-    },
-  ],
+  workflow: [],
 };
